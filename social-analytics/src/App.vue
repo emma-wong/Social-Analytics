a889f08f5fd9b11a940deb2487063f74b6caebc7
ogparra <ogparra.15@gmail.com>
2018-07-18 17:29:51 -0700

    added login textfield

--- social-analytics/src/App.vue
+++ social-analytics/src/App.vue
@@ -1,36 +1,38 @@
 <template>
-  <div id="app">
-    <Language-API/>
-    <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
-  </div>
+    <div id="app">
+       <!--  <Language-API/> -->
+        <Signup/>
+        
+        <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
+    </div>
 </template>
-
 <script>
 import Vue from 'vue'
 import axios from 'axios'
 import HelloWorld from './components/HelloWorld.vue'
-import LanguageAPI from './components/LanguageAPI.vue'
+// import LanguageAPI from './components/LanguageAPI.vue'
+import Signup from './components/Signup.vue'
 
 Vue.prototype.$http = axios;
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+    name: 'app',
+    components: {
+        Signup
+    }
 }
 
-Vue.component("Language-API", LanguageAPI);
-</script>
+Vue.component("Hello-World", HelloWorld);
 
+</script>
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-  font-size: 20px;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+    font-size: 20px;
 }
 </style>
---
+++ social-analytics/src/components/Signup.vue
@@ -0,0 +1,31 @@
+
+<template>
+    <div>
+        <h1> Welcome to Social Analytics! </h1>
+        <h3> Signup </h3>   
+        <div>
+            <label> Name  </label>
+            <input type="text" name="name" v-model="name">  <br>
+            <label>Email </label>
+            <input type="text" name="email"> <br>
+            <label> Password  </label>
+            <input type="text" name="password">
+        </div>
+        {{ name }}
+        {{ email }}
+        {{ password }}
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Signup", 
+    data: function(){
+        return {
+            name: "Name", 
+            email: "Email", 
+            password: "Password"
+        }
+    }
+}
+</script>
